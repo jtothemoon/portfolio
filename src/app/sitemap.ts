db0d2c7a663346946ad4360f67592e731e2cef8f
Jtothemoon <jtothemoon@naver.com>
2025-09-20 13:07:51 +0900

    lint 에러 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,4 @@
-import { MetadataRoute } from 'next'
+import { type MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://limhyunjin.com'
@@ -8,25 +8,25 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: baseUrl,
       lastModified: new Date(),
       changeFrequency: 'yearly',
-      priority: 1,
+      priority: 1
     },
     {
       url: `${baseUrl}/projects`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.8,
+      priority: 0.8
     },
     {
       url: `${baseUrl}/career`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.8,
+      priority: 0.8
     },
     {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.5,
-    },
+      priority: 0.5
+    }
   ]
 }
